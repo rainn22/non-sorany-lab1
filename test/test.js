8070ba0b2e2dc2f6389e11eda048969d8d6033ba
rainn22 <nonsorany859@gmail.com>
2025-11-03 12:24:22 +0700

    update test message and compare

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ const app = require('../app');
 const { expect } = require('chai');
 
 describe('GET /', () => {
-    it('should return Hello, GitHub Actions!', (async () => {
+    it('should return Hello, CI/CD!', (async () => {
         const res = await request(app)
             .get('/');
         expect(res.status).to.equal(200);
-        expect(res.text).to.equal('Hello, GitHub Actions!');
+        expect(res.text).to.equal('Hello, CI/CD!');
     }));
 });
