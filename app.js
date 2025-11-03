737170ec8f233689ceb8244fc74cccf66bef1dd3
rain <rany@Rain.local>
2025-11-03 12:16:32 +0700

    Update greeting message

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, GitHub Actions!');
+    res.send('Hello, CI/CD!');
 });
 module.exports = app;
